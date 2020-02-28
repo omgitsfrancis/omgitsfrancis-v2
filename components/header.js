445badd07d8d22af575f0a4d92995fc3356b9906
omgitsfrancis <omgitsfrancis@gmail.com>
2020-02-27 19:44:34 -0800

    Updated header content

--- components/header.js
+++ components/header.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowDown,
+  faEnvelopeSquare,
+  faFile
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { useState } from "react";
 
 const Container = styled.div`
@@ -27,25 +32,100 @@ const Container = styled.div`
 const Title = styled.h1`
   font-size: 72px;
   text-align: center;
+  white-space: nowrap;
+
+  @media (max-width: 768px) {
+    font-size: 42px;
+  }
+`;
+
+const HoverText = styled.span`
+  &:hover {
+    cursor: default;
+    opacity: 0.7;
+  }
+`;
+
+const SocialMediaContainer = styled.div`
+  position: absolute;
+  right: 0;
+
+  a:not(:last-child) {
+    margin-right: 0.5rem;
+  }
+`;
+
+const SocialMediaIcon = styled(FontAwesomeIcon)`
+  transition: transform 0.1s;
+
+  &:hover {
+    transform: scale(1.5);
+    opacity: 0.7;
+  }
+
+  @keyframes bounce {
+    from {
+      transform: translateY(0px);
+    }
+    50% {
+      transform: translateY(-10px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
 `;
 
 const ViewMoreIcon = styled(FontAwesomeIcon)`
   cursor: pointer;
   transition: transform 0.25s ease 0s;
   &:hover {
-    transform: translateY(5px);
+    transform: translateY(10px);
+    opacity: 0.7;
   }
-`
+`;
 
 export default function Header(props) {
   return (
     <Container>
-      <div>
-        <p>Hello, I'm</p>
-        <Title>Francis Enriquez</Title>
+      <div style={{ position: "relative" }}>
+        <SocialMediaContainer>
+          <a href="https://github.com/omgitsfrancis/">
+            <SocialMediaIcon
+              icon={faGithubSquare}
+              style={{ animation: "bounce 0.2s linear 1s" }}
+              size="sm"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/francis-enriquez/">
+            <SocialMediaIcon
+              icon={faLinkedin}
+              style={{ animation: "bounce 0.2s linear 1.1s" }}
+              size="sm"
+            />
+          </a>
+          <a href="mailto:francisdenriquez@gmail.com">
+            <SocialMediaIcon
+              icon={faEnvelopeSquare}
+              style={{ animation: "bounce 0.2s linear 1.2s" }}
+              size="sm"
+            />
+          </a>
+        </SocialMediaContainer>
+        <p>
+          <HoverText>Hello, </HoverText>
+          <HoverText>I'm</HoverText>
+        </p>
+        <Title>
+          {"Francis Enriquez".split("").map((letter, index) => {
+            return <HoverText key={index}>{letter}</HoverText>;
+          })}
+        </Title>
       </div>
       <p style={{ marginBottom: "3rem" }}>
-        tester, developer, and all of the above
+        {['tester, ', 'developer, ', 'and, ', 'all ','of ', 'the ', 'above'].map((word, index) => {
+          return <HoverText key={index}>{word}</HoverText>
+        })}
       </p>
       <a href="#about">
         <ViewMoreIcon icon={faArrowDown} size="4x" />
