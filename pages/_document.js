3fecf46ef776ac9dce36b3eb717a7ddb3f663195
omgitsfrancis <omgitsfrancis@gmail.com>
2020-03-08 22:51:43 -0700

    Added title

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,36 @@
-import Document from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import Head from "next/head";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-        })
-      const initialProps = await Document.getInitialProps(ctx)
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        });
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
           <>
+            <Head>
+              <title>Francis Enriquez</title>
+              <meta
+                name="viewport"
+                content="initial-scale=1.0, width=device-width"
+              />
+            </Head>
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        ),
-      }
+        )
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 }
