f5290cc10a7401da8305855b650794008e72a4cc
omgitsfrancis <omgitsfrancis@gmail.com>
2020-03-08 22:42:35 -0700

    Added hover icon effect

--- components/project.js
+++ components/project.js
@@ -8,12 +8,12 @@ const ProjectContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: calc(33% - 1rem);
-	margin-bottom: 1.5rem;
-  
-	@media (max-width: 1024px) {
-		width: calc(50% - 1rem);
+  margin-bottom: 1.5rem;
+
+  @media (max-width: 1024px) {
+    width: calc(50% - 1rem);
   }
-	@media (max-width: 768px) {
+  @media (max-width: 768px) {
     width: calc(100% - 1rem);
   }
 `;
@@ -26,16 +26,25 @@ const ProjectHeader = styled.div`
 `;
 
 const Title = styled.span`
-	font-size: 18px;
-`
+  font-size: 18px;
+  text-overflow: ellipsis;
+  font-weight: 500;
+`;
 
-const Description =  styled.p`
-	font-size: 14px;
-`
+const Description = styled.p`
+  font-size: 14px;
+`;
 
 const ProjectBody = styled.div`
   padding: 0.5rem;
 `;
+
+const Icon = styled(FontAwesomeIcon)`
+  cursor: pointer;
+  &:hover {
+    opacity: 0.7;
+  }
+`
 
 export default function Project({
   title,
@@ -48,20 +57,22 @@ export default function Project({
   return (
     <ProjectContainer>
       <ProjectHeader>
-        <Title style={{textOverflow: "ellipsis"}}>{title.replace(/-/g, " ")}</Title>
+        <Title>
+          {title}
+        </Title>
         <div>
-          <a href={gitLink}>
-            <FontAwesomeIcon icon={faGithub} size="lg" />
-          </a>
           {projectLink && (
             <a href={projectLink}>
-              <FontAwesomeIcon
-                style={{ marginLeft: "0.5rem" }}
+              <Icon
+                style={{ marginRight: "0.5rem" }}
                 icon={faExternalLinkAlt}
                 size="lg"
               />
             </a>
           )}
+          <a href={gitLink}>
+            <Icon icon={faGithub} size="lg" />
+          </a>
         </div>
       </ProjectHeader>
       <ProjectBody>
